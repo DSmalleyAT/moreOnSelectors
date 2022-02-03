a831e4ee8c7c39b9fdccea2370352567b7fe101c
MacMini17 <mcm-mac-17@MacMini17s-Mac-mini.local>
2022-02-03 10:20:17 -0800

    Finished part 4

---
+++ test/pageobjects/form.page.js
@@ -0,0 +1,48 @@
+const Page = require("./page");
+
+class FormPage extends Page {
+     
+     //define selectors using getter methods
+     
+     // Header Selector
+     get header() {
+          return $('h1=Form Fields'); //uses Element with certain text selector
+     }
+
+     // Email Selector
+     get inputEmail() {
+          return $("#mail"); //uses CSS query selector
+     }
+
+     // Error text
+    get errorText() {
+        return $('.error-text')
+    }
+    
+    //The one I (Daniel S) pasted in manually for the dropdown menu
+    get interest() {
+        return $('#title option:nth-child(3)'); // //uses CSS query selector (class)
+   }
+     
+     // Submit Button Selector
+     get submit() {
+          return $('button=Register'); //uses Element with certain text selector
+     }
+     /**
+      * a method to encapsule automation code to interact with the page
+      * e.g. to login using username and password
+      */
+     async email() {
+          await this.inputEmail.setValue(input);
+          await this.submit.click();
+     }
+
+     /**
+      * overwrite specific options to adapt it to page object
+      */
+     open() {
+          return super.open('');
+     }
+}
+
+module.exports = new FormPage();
